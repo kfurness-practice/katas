85f87f2855e27186d727da798fea365de8b9ed94
kelly <kelly.n.waite@gmail.com>
2017-02-08 12:57:02 -0700

    Pass test7

--- war/wartest.js
+++ war/wartest.js
@@ -55,13 +55,47 @@ test('5: A game of card1 being King and card2 being 2 should return card1 as the
   t.is(actual, expected)
 })
 
+test('6: A game of card1 being Queen and card2 being King should return card2 as the winner', t=> {
+  const expected = "Card2 Wins"
+
+  const card1 = 'queen'
+  const card2 = 'king'
+
+  const actual = war.round(card1, card2)
+
+  t.is(actual, expected)
+})
+
+test('7 A game of card1 being King and card2 being Ace should return card2 as the winner', t=> {
+  const expected = "Card2 Wins"
+
+  const card1 = 'king'
+  const card2 = 'ace'
+
+  const actual = war.round(card1, card2)
+
+  t.is(actual, expected)
+})
+
 const war = {
   round(card1, card2) {
-    if( card1 === 'king') {
-      return 'Card1 Wins'
-    }
-    const card1Int = Number(card1)
-    const card2Int = Number(card2)
-    return card1Int > card2Int ? 'Card1 Wins' : 'Card2 Wins'
+    const arr = []
+    arr.push(card1, card2)
+    const normalize = arr.map((card) => {
+      switch (card) {
+        case 'ace' :
+          return 14
+        case 'king' :
+          return 13
+        case 'queen' :
+          return 12
+        case 'jack' :
+          return 11
+        default :
+          return Number(card)
+      }
+    })
+
+    return normalize[0] > normalize[1] ? 'Card1 Wins' : 'Card2 Wins'
   }
 }
