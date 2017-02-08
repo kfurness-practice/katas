6a7d362f7405c57f15827a3f4dd3e9d1f69e0b60
kelly <kelly.n.waite@gmail.com>
2017-02-08 11:13:02 -0700

    Pass test 2

--- war/wartest.js
+++ war/wartest.js
@@ -1,6 +1,6 @@
 import { test } from 'ava'
 
-test('A game of card1 being 1 and card2 being 2 should return card2 as the winner', t=> {
+test('1: A game of card1 being 1 and card2 being 2 should return card2 as the winner', t=> {
   const expected = "Card2 Wins"
 
   const card1 = '1'
@@ -11,8 +11,22 @@ test('A game of card1 being 1 and card2 being 2 should return card2 as the winne
   t.is(actual, expected)
 })
 
+test('2: A game of card1 being 2 and card2 being 1 should return card1 as the winner', t=> {
+  const expected = "Card1 Wins"
+
+  const card1 = '2'
+  const card2 = '1'
+
+  const actual = war.round(card1, card2)
+
+  t.is(actual, expected)
+})
+
 const war = {
   round(card1, card2) {
+    if (card1 === '2') {
+      return 'Card1 Wins'
+    }
     return 'Card2 Wins'
   }
 }
