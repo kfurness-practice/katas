dfc7cc919182a50db0b3002b69b383160bbdcdfe
kelly <kelly.n.waite@gmail.com>
2017-02-08 10:24:37 -0700

    Passed test 9

--- hamming/test.js
+++ hamming/test.js
@@ -88,11 +88,23 @@ test('8: First string of "ACCAGGG" and a second string of "ACTATGG" should retur
   t.is(actual, expected)
 })
 
+test('9: First string of "GGGHHHHEEEEEEEEEEEE" and a second string of "A" should return 0', t => {
+  const expected = 0
+
+  const strand1 = 'GGGHHHHEEEEEEEEEEEE'
+  const strand2 = 'A'
+
+  const actual = dna.hammingDistance(strand1, strand2);
+
+  t.is(actual, expected)
+})
+
 
 const dna = {
   hammingDistance(strand1, strand2) {
     let mutations = 0
     for(let i = 0; i < strand1.length; i++) {
+      if( strand1.length != strand2.length ) return mutations
      strand1[i] != strand2[i] ? mutations++ : mutations
     }
     return mutations
