75a36dfcb2d4b2d070a4061d0abafd9da758dce2
kelly <kelly.n.waite@gmail.com>
2017-02-08 10:01:18 -0700

    Passed 3rd test

--- hamming/test.js
+++ hamming/test.js
@@ -11,7 +11,7 @@ test('Two empty strings should return 0', t => {
   t.is(actual, expected)
 })
 
-test('A string with "A" should return 1', t => {
+test('First string of "A" and a second string of "B" should return 1', t => {
   const expected = 1
 
   const strand1 = "A"
@@ -22,9 +22,20 @@ test('A string with "A" should return 1', t => {
   t.is(actual, expected)
 })
 
+test('A string with "A" should return 1', t => {
+  const expected = 0
+
+  const strand1 = "A"
+  const strand2 = 'A'
+
+  const actual = dna.hammingDistance(strand1, strand2);
+
+  t.is(actual, expected)
+})
+
 const dna = {
   hammingDistance(strand1, strand2) {
-    if(strand1 === "A") {
+    if(strand2 === "B") {
       return 1
     }
     return 0
