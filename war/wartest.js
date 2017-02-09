d8ed6140f5df3f626f9ebe9f19e557827008f832
kelly <kelly.n.waite@gmail.com>
2017-02-09 16:46:12 -0700

    Passed test 3b

--- war/wartest.js
+++ war/wartest.js
@@ -104,8 +104,31 @@ test('2b: Should split the deck in half and give player1 26 cards', t=> {
   t.is(actual, expected)
 })
 
+test('3b: Should randomize the deck of cards', t=> {
+  const expected = war.deck
+
+  const actual = war.randomizeDeck(war.deck)
+
+  console.log('actual', actual);
+  console.log(expected);
+
+  t.not(actual, expected)
+})
+
+
 const war = {
   deck: ['Ace of Clubs', '2 of Clubs', '3 of Clubs', '4 of Clubs', '5 of Clubs', '6 of Clubs', '7 of Clubs', '8 of Clubs', '9 of Clubs','10 of Clubs', 'Jack of Clubs', 'Queen of Clubs', 'King of Clubs',  'Ace of Hearts', '2 of Hearts', '3 of Hearts', '4 of Hearts', '5 of Hearts', '6 of Hearts', '7 of Hearts', '8 of Hearts', '9 of Hearts', '10 of Hearts', 'Jack of Hearts', 'Queen of Hearts', 'King of Hearts', 'Ace of Diamonds', '2 of Diamonds', '3 of Diamonds', '4 of Diamonds', '5 of Diamonds', '6 of Diamonds', '7 of Diamonds', '8 of Diamonds', '9 of Diamonds', '10 of Diamonds', 'Jack of Diamonds', 'Queen of Diamonds', 'King of Diamonds', 'Ace of Clubs', '2 of Clubs', '3 of Clubs', '4 of Clubs', '5 of Clubs', '6 of Clubs', '7 of Clubs', '8 of Clubs', '9 of Clubs', '10 of Clubs', 'Jack of Clubs', 'Queen of Clubs', 'King of Clubs'],
+
+  randomizeDeck: function(deck) {
+    const randomDeck = deck.slice()
+    for (var i = randomDeck.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = randomDeck[i];
+        randomDeck[i] = randomDeck[j];
+        randomDeck[j] = temp;
+    }
+    return randomDeck;
+  },
 
   player1Cards: function() {
     return war.deck.slice(0, 26)
