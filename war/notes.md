ddec112b3f9dbadc9802d0957bfa6e9bd9cbc8ba
kelly <kelly.n.waite@gmail.com>
2017-02-09 15:20:13 -0700

    Pass test 8

--- war/notes.md
+++ war/notes.md
@@ -6,3 +6,6 @@
 ## What was hard
 * Deciding how to even start the game was really difficult. For example, I had no one saying, you will get an array of the player's turns that represent the game. I didn't know what information I would be getting because I had to come up with it on my own.
 * In relation to above, I didn't know what my first test should be because I did not even know how to organize the initial information. Is the first thing I do dividing the cards equally amongst players? Is it just deciding who wins a simple example?
+
+## Steps I took
+1. Created a function to see who won a single round of war. 
--- war/wartest.js
+++ war/wartest.js
@@ -1,7 +1,7 @@
 import { test } from 'ava'
 
 test('1: A game of card1 being 1 and card2 being 2 should return card2 as the winner', t=> {
-  const expected = "Card2 Wins"
+  const expected = "Card 2 Wins"
 
   const card1 = '1'
   const card2 = '2'
@@ -12,7 +12,7 @@ test('1: A game of card1 being 1 and card2 being 2 should return card2 as the wi
 })
 
 test('2: A game of card1 being 2 and card2 being 1 should return card1 as the winner', t=> {
-  const expected = "Card1 Wins"
+  const expected = "Card 1 Wins"
 
   const card1 = '2'
   const card2 = '1'
@@ -23,7 +23,7 @@ test('2: A game of card1 being 2 and card2 being 1 should return card1 as the wi
 })
 
 test('3: A game of card1 being 5 and card2 being 1 should return card1 as the winner', t=> {
-  const expected = "Card1 Wins"
+  const expected = "Card 1 Wins"
 
   const card1 = '5'
   const card2 = '1'
@@ -34,7 +34,7 @@ test('3: A game of card1 being 5 and card2 being 1 should return card1 as the wi
 })
 
 test('4: A game of card1 being 3 and card2 being 2 should return card1 as the winner', t=> {
-  const expected = "Card1 Wins"
+  const expected = "Card 1 Wins"
 
   const card1 = '3'
   const card2 = '2'
@@ -45,9 +45,9 @@ test('4: A game of card1 being 3 and card2 being 2 should return card1 as the wi
 })
 
 test('5: A game of card1 being King and card2 being 2 should return card1 as the winner', t=> {
-  const expected = "Card1 Wins"
+  const expected = "Card 1 Wins"
 
-  const card1 = 'king'
+  const card1 = 'King'
   const card2 = '2'
 
   const actual = war.round(card1, card2)
@@ -56,10 +56,10 @@ test('5: A game of card1 being King and card2 being 2 should return card1 as the
 })
 
 test('6: A game of card1 being Queen and card2 being King should return card2 as the winner', t=> {
-  const expected = "Card2 Wins"
+  const expected = "Card 2 Wins"
 
-  const card1 = 'queen'
-  const card2 = 'king'
+  const card1 = 'Queen'
+  const card2 = 'King'
 
   const actual = war.round(card1, card2)
 
@@ -67,10 +67,21 @@ test('6: A game of card1 being Queen and card2 being King should return card2 as
 })
 
 test('7 A game of card1 being King and card2 being Ace should return card2 as the winner', t=> {
-  const expected = "Card2 Wins"
+  const expected = "Card 2 Wins"
 
-  const card1 = 'king'
-  const card2 = 'ace'
+  const card1 = 'King'
+  const card2 = 'Ace'
+
+  const actual = war.round(card1, card2)
+
+  t.is(actual, expected)
+})
+
+test('8 A game of card1 being 8and card2 being 8 should return call for a tie breaker function', t=> {
+  const expected = undefined
+
+  const card1 = '8'
+  const card2 = '8'
 
   const actual = war.round(card1, card2)
 
@@ -78,24 +89,30 @@ test('7 A game of card1 being King and card2 being Ace should return card2 as th
 })
 
 const war = {
-  round(card1, card2) {
+  tieBreaker: function() {
+    return
+  },
+  round: function(card1, card2) {
     const arr = []
     arr.push(card1, card2)
     const normalize = arr.map((card) => {
       switch (card) {
-        case 'ace' :
+        case 'Ace' :
           return 14
-        case 'king' :
+        case 'King' :
           return 13
-        case 'queen' :
+        case 'Queen' :
           return 12
-        case 'jack' :
+        case 'Jack' :
           return 11
         default :
           return Number(card)
       }
     })
-
-    return normalize[0] > normalize[1] ? 'Card1 Wins' : 'Card2 Wins'
+    if( normalize[0] === normalize[1] ) {
+      war.tieBreaker()
+      return
+    }
+    return normalize[0] > normalize[1] ? 'Card 1 Wins' : 'Card 2 Wins'
   }
 }
