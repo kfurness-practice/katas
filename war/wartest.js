0df86a71f17ada53beaf94bb116ba868fb82357f
kelly <kelly.n.waite@gmail.com>
2017-02-09 16:18:44 -0700

    Pass test 2b

--- war/wartest.js
+++ war/wartest.js
@@ -96,8 +96,20 @@ test('1b: Should contain a deck of 52 cards', t=> {
   t.is(actual, expected)
 })
 
+test('2b: Should split the deck in half', t=> {
+  const expected = 26
+
+  const actual = war.player1Cards().length
+
+  t.is(actual, expected)
+})
+
 const war = {
   deck: ['Ace of Clubs', '2 of Clubs', '3 of Clubs', '4 of Clubs', '5 of Clubs', '6 of Clubs', '7 of Clubs', '8 of Clubs', '9 of Clubs','10 of Clubs', 'Jack of Clubs', 'Queen of Clubs', 'King of Clubs',  'Ace of Hearts', '2 of Hearts', '3 of Hearts', '4 of Hearts', '5 of Hearts', '6 of Hearts', '7 of Hearts', '8 of Hearts', '9 of Hearts', '10 of Hearts', 'Jack of Hearts', 'Queen of Hearts', 'King of Hearts', 'Ace of Diamonds', '2 of Diamonds', '3 of Diamonds', '4 of Diamonds', '5 of Diamonds', '6 of Diamonds', '7 of Diamonds', '8 of Diamonds', '9 of Diamonds', '10 of Diamonds', 'Jack of Diamonds', 'Queen of Diamonds', 'King of Diamonds', 'Ace of Clubs', '2 of Clubs', '3 of Clubs', '4 of Clubs', '5 of Clubs', '6 of Clubs', '7 of Clubs', '8 of Clubs', '9 of Clubs', '10 of Clubs', 'Jack of Clubs', 'Queen of Clubs', 'King of Clubs'],
+
+  player1Cards: function() {
+    return war.deck.splice(0, 26)
+  },
 
   tieBreaker: function() {
     return
